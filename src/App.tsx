02cca45b09d621a55ee8605b7e6e762c8c1bd4aa
bellDev-code <sumaoo20@naver.com>
2023-09-11 19:00:50 +0900

    react-router v6 & styled-components 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Layouts/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <div>clone-coding</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+const HomePage = () => {
+    return (
+        <Container>홈페이지</Container>
+    )
+}
+
+export default HomePage;
